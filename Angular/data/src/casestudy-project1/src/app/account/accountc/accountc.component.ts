7801e9c9d04e0dcbd927923a7765521ed4b57168
aparnamachavolu23 <60041379+aparnamachavolu23@users.noreply.github.com>
2020-02-01 17:59:39 +0530

    Add files via upload

---
+++ Angular/data/src/casestudy-project1/src/app/account/accountc/accountc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accountc',
+  templateUrl: './accountc.component.html',
+  styleUrls: ['./accountc.component.css']
+})
+export class AccountcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Angular/data/src/casestudy-project1/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './login/register/register.component';
+import { LoginuComponent } from './login/loginu/loginu.component';
+import { MultimediaComponent } from './media/multimedia/multimedia.component';
+import { SinglemediaComponent } from './media/singlemedia/singlemedia.component';
+import { MymediaComponent } from './media/mymedia/mymedia.component';
+import { GalleryComponent } from './media/gallery/gallery.component';
+import { UploadmediaComponent } from './media/uploadmedia/uploadmedia.component';
+const routes: Routes = [
+  {  path:"", redirectTo : "login", pathMatch: "full"},
+  { path:"register", component: RegisterComponent},
+  { path:"login", component: LoginuComponent},
+  { path:"singlemedia",component :SinglemediaComponent},
+  { path:"multimedia",component :MultimediaComponent},
+  { path:"mymedia", component:MymediaComponent},
+  { path:"gallery",component:GalleryComponent},
+  { path:"uploadmedia",component:UploadmediaComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
