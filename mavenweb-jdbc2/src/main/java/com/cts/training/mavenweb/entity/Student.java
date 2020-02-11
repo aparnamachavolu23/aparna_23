5aec4df3956770bb2bbcfce6cf7d2fdd9022eff0
aparnamachavolu23 <60041379+aparnamachavolu23@users.noreply.github.com>
2020-02-11 18:03:46 +0530

    Add files via upload

---
+++ mavenweb-jdbc2/src/main/java/com/cts/training/mavenweb/entity/Student.java
@@ -0,0 +1,77 @@
+package com.cts.training.mavenweb.entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+/*@Getter
+@Setter
+@AllArgsConstructor
+@ToString*/
+public class Student {
+
+	private Integer id;
+	private String name;
+	private String email;
+	private Integer age;
+	
+	public Student() {}
+
+	
+	public Student(Integer id, String name, String email,Integer age) {
+		
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.age=age;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setAge(Integer age) {
+		this.age= age;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", email=" + email + ",age=" +age+ "]";
+	}
+	
+	
+	
+}
